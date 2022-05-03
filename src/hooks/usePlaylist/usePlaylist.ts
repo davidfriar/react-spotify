5661c4886184edee7f2086c6093410ce60d499a5
David Friar <david.friar@cognifide.com>
2022-05-03 09:46:14 +0100

    remove spurious comments(

--- src/hooks/usePlaylist/usePlaylist.ts
+++ src/hooks/usePlaylist/usePlaylist.ts
@@ -4,14 +4,6 @@ import { useState } from "react"
 import { OpArgType } from "openapi-typescript-fetch"
 import { Playlist, Id, Token } from "../../types"
 
-// type Path = paths["/playlists/{playlist_id}"]
-// type Method = "get"
-// type RequiredParams = "playlist_id"
-// type Params = OpArgType<Path[Method]>
-// type OptionalParams = Omit<Params, RequiredParams>
-// type ReturnType = OpReturnType<Path[Method]>
-// type TargetReturnType = Playlist
-
 export type UsePlaylistParams = Omit<
   OpArgType<paths["/playlists/{playlist_id}"]["get"]>,
   "playlist_id"
